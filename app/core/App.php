0c4756bdb5440519b6e76ecb761ad3169a4d698c
fmrozaqi <faiz.rozaqi@gmail.com>
2020-12-07 10:27:57 +0700

    test

--- app/core/App.php
+++ app/core/App.php
@@ -36,7 +36,7 @@ class App {
         }
 
         // jalankan controller & method, serta kirimkan params jika ada
-        call_user_func_array([$this->controller, $this->method], $this->params);
+        //call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
