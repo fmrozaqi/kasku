70c3f8d02194f11d1822d6ebe8b30e64a7122a2c
root <root@ip-172-31-94-140.ec2.internal>
2020-12-07 03:18:09 +0000

    edit ec2

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-define('BASEURL', 'http://localhost/kasku');
+define('BASEURL', 'http://ec2-34-227-159-124.compute-1.amazonaws.com/kasku');
 
 // DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'kasku-db.cfbujcetickx.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'admin');
+define('DB_PASS', '12345678');
 define('DB_NAME', 'kasku_mvc');
